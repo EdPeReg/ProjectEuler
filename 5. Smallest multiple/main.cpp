84f3a2dfdd0ddaa349135670eb49c21aa15997e3
EdPeReg <dardo1818@gmail.com>
2020-08-12 11:05:20 -0500

    First version

--- 5. Smallest multiple/main.cpp
+++ 5. Smallest multiple/main.cpp
@@ -1,23 +1,21 @@
 // https://projecteuler.net/problem=5
 
-/* We start from 20 because we will divide from 1-20, if our number is
- * 15, any number above that will be invalid (we need to reach 20, is our limit).
+/* We start from 20 because we will divide from 2-20, (1 is not taken in consideration because any number divided by 1
+ * will always yield a remainder 0), if our number is 15, any number above that will be invalid (we need to reach 20, is our limit).
  *
  * We start like this:
- * 20 % 1 = 0
  * 20 % 2 = 0
  * 20 % 3 = 2 -> We increment 20 by 1 and we will start again from 1-20.
  *
- * 21 % 1 = 0
  * 21 % 2 = 1 -> Increment 21 by 1 and start again from 1.
  *
- * We continue doing this until we find a number that is divisible from 1-20, once we
+ * We continue doing this until we find a number that is divisible from 2-20, once we
  * hit 20, we stop. */
 
 #include <iostream>
 
 int main() {
-	int num = 1, num2 = 20;
+	int num = 2, num2 = 20;
 	while(num <= 20) {
 		if(num2 % num != 0) {
 			++num2;
